f0f4122022ab371c5c65f79d1fdc96996d10c68b
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-05-04 18:28:32 +0530

    still can not generate bss section

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 
 uint16_t blink_count;
 
-uint16_t delay_cycles = 100;
+uint16_t base_delay_cycles = 100;
 
 void delay_for(unsigned int clocks)
 {
@@ -10,6 +10,28 @@ void delay_for(unsigned int clocks)
 	{
 		asm("nop");
 	}
+}
+
+uint16_t multiply(uint16_t a, uint16_t b)
+{
+	volatile uint16_t multiplied = 0;
+	volatile uint16_t i;
+	for(i = 0; i < a; i++)
+	{
+		multiplied += b;
+	}
+	return multiplied;
+}
+
+void increment_blink_count()
+{
+	blink_count++;
+	blink_count = (blink_count > 655) ? 0 : blink_count;
+}
+
+uint16_t get_prescribed_delay()
+{
+	return multiply(blink_count, base_delay_cycles);
 }
 
 void main()
@@ -22,18 +44,10 @@ void main()
 		// led GPIO toggle
 		GPIOB->PORT ^= 1<<4;
 
-		// increment blink count to know, the number of timer we blinked
-		blink_count++;
-		blink_count = (blink_count > 655) ? 0 : blink_count;
-
-		volatile uint16_t multiplied = 0;
-		volatile uint16_t i;
-		for(i = 0; i < blink_count; i++)
-		{
-			multiplied += delay_cycles;
-		}
+		// increment blink count to know, the number of times we blinked
+		increment_blink_count();
 
 		// delay
-		delay_for(multiplied);
+		delay_for(get_prescribed_delay());
 	}
 }
